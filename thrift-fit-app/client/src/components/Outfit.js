14f38c4eab8102ef7332b6445360e66633a42203
Sarah Rourke <sarahrourke@Sarahs-MBP.attlocal.net>
2020-08-28 15:18:26 -0500

    added outfit.js

---
+++ thrift-fit-app/client/src/components/Outfit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Outfit = (props) => {
+    return (
+        <div className="outfit">
+            <h2>outfit props go here props.outfit.data</h2>
+        </div>
+    );
+};
+
+export default Outfit;
