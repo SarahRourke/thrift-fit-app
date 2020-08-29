53970a358f9e028ba76b5ac076d79a2181760091
Sarah Rourke <sarahrourke@Sarahs-MBP.attlocal.net>
2020-08-29 16:08:10 -0500

    alloutfits edited

--- thrift-fit-app/client/src/components/AllOutfits.js
+++ thrift-fit-app/client/src/components/AllOutfits.js
@@ -1,65 +1,78 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Outfit from './Outfit';
-class AllOutfits extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            outfits: null,
-            dataLoaded: false,
-            auth: props.auth,
-        }
-    }
 
-    componentDidMount() {
-        this.getAllOutfits();
-    } 
-    
-    getAllOutfits() {
-        fetch('/api/outfits'/*,{ credentials: 'include' }*/)
-        .then(res => res.json())
-        .then(res => {
-            this.setState({
-                outfits: res.data.outfits,
-                dataLoaded: true,
-            })
-        }).catch(err => console.log(err));
-    }
 
-    handleFormSubmit(method, e, data, id) {
-        e.preventDefault()
-        fetch(`/api/outfits/${id || ''}`, {
-            method: method,
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data),
-        })
-        .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            this.getAllOutfits();
-        }).catch(err => console.log(err));
-    }
-
-    renderAllOutfits() {
-        if (this.state.dataLoaded) {
-            return this.state.outfits.map(outfit => {
-                return <Outfit key={outfit.id} outfit={outfit} />
-            })
-        } if (this.state.auth === true) {
-            return this.handleFormSubmit(isAdd === true);
-        }
-        else return <p>Cleaning our closet...</p>;
-    }
-
-    render() {
-        return (
-            <div className="alloutfits">
-                {this.renderAllOutfits()}
-            </div>
-        )
-    }
-}
+const AllOutfits = (props) => {
+    return (
+        <div className="allOutfits">
+            {props.allOutfits.map(outfit => {
+                return <Outfit key={outfit.id} outfitt={outfit} />
+            })}
+        </div>
+    );
+};
 
 export default AllOutfits;
+// class AllOutfits extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             outfits: null,
+//             dataLoaded: false,
+//             auth: props.auth,
+//         }
+//     }
+
+//     componentDidMount() {
+//         this.getAllOutfits();
+//     } 
+    
+//     getAllOutfits() {
+//         fetch('/api/outfits'/*,{ credentials: 'include' }*/)
+//         .then(res => res.json())
+//         .then(res => {
+//             this.setState({
+//                 outfits: res.data.outfits,
+//                 dataLoaded: true,
+//             })
+//         }).catch(err => console.log(err));
+//     }
+
+//     handleFormSubmit(method, e, data, id) {
+//         e.preventDefault()
+//         fetch(`/api/outfits/${id || ''}`, {
+//             method: method,
+//             credentials: 'include',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(data),
+//         })
+//         .then(res => res.json())
+//         .then(res => {
+//             console.log(res);
+//             this.getAllOutfits();
+//         }).catch(err => console.log(err));
+//     }
+
+//     renderAllOutfits() {
+//         if (this.state.dataLoaded) {
+//             return this.state.outfits.map(outfit => {
+//                 return <Outfit key={outfit.id} outfit={outfit} />
+//             })
+//         } if (this.state.auth === true) {
+//             return this.handleFormSubmit(isAdd === true);
+//         }
+//         else return <p>Cleaning our closet...</p>;
+//     }
+
+//     render() {
+//         return (
+//             <div className="alloutfits">
+//                 {this.renderAllOutfits()}
+//             </div>
+//         )
+//     }
+// }
+
+// export default AllOutfits;
