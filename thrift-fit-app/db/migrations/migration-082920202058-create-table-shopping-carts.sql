40d50afa3808fd7375da1dd48ed31e4fac37064c
Shirley Hastier <shastier@yahoo.com>
2020-08-30 17:53:10 -0500

    Migrations, updated shopping_carts table per schema

--- thrift-fit-app/db/migrations/migration-082920202058-create-table-shopping-carts.sql
+++
@@ -1,6 +0,0 @@
-  
-CREATE TABLE IF NOT EXISTS shopping_carts (
-	id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-	is_sale_completed BOOLEAN DEFAULT 'false'   
-);
--- thrift-fit-app/db/migrations/migration-082920202119-create-table-shopping_cart_items.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS shopping_cart_items (
-	id SERIAL PRIMARY KEY,
-    shopping_cart_id INTEGER NOT NULL,
-    item_id INTEGER,
-    FOREIGN KEY (shopping_cart_id) REFERENCES shopping_carts(id),
-    FOREIGN KEY (item_id) REFERENCES outfits(id)
-);
