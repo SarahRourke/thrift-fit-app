a6eaa8e9dcb533ec59e3f4cea8226acd21343b1a
Sarah Rourke <sarahrourke@Sarahs-MBP.attlocal.net>
2020-09-01 11:55:29 -0500

    navbar styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,19 +9,26 @@ const Header = (props) => {
     <header className="header">
       <div className="logo">Thrift-Fit-App</div>
       <SearchBarForm />
-      <div class="navbar">
-        <a><Link to="/">Home</Link></a>
-        <a><Link to="/login">Login</Link></a>
-        <a><Link to="/register">Register</Link></a>
+        <div class="navbar">
+          <a><Link to="/">Home</Link></a>
+            <div class="dropdown">
+              <button class="dropbtn">Profile
+                <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-content">
+                  <a><Link to="/login">Login</Link></a> <a><Link to="/dashboard">Dashboard</Link></a>
+                  <a><Link to="/register">Register</Link></a>
+                  <a onClick={props.logout}><Link to="/">Logout</Link></a>
+                </div>
+            </div>
         <a><Link to="/shopping-cart">Shopping Cart</Link></a>
         <div class="dropdown">
-          <button class="dropbtn">Dropdown
+          <button class="dropbtn">Outfits
           <i class="fa fa-caret-down"></i>
           </button>
-          <div class="dopdown-content">
-              <a><Link to="/dashboard">Dashboard</Link></a>
-              <a><Link to="/outfits">Outfits</Link></a>
-              <a><span onClick={props.logout}>Logout</span></a>
+          <div class="dropdown-content">
+              <a><Link to="/outfits">All Outfits</Link></a>
+              
           </div>
         </div> 
       </div>
