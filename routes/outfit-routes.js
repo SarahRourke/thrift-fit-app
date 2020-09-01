18cd71a7e511c5d21b10f44a44ef2110909e271a
Shirley Hastier <shastier@yahoo.com>
2020-09-01 14:36:49 -0500

    Routes, api/outfits/, updated show outfit route comment

--- routes/outfit-routes.js
+++ routes/outfit-routes.js
@@ -7,7 +7,7 @@ const authHelpers = require('../services/auth/auth-helpers');
 // root route, /api/outfits
 outfitRoutes.get('/', outfitsController.index);
 
-// show details for an specific outfit route, /api/outfits/:id
+// show details for an specific outfit route, /api/outfits/outfit/:id
 outfitRoutes.get('/outfit/:id', outfitsController.show);
 
 // add new outfit route, /api/outfits/add 
