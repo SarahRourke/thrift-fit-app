de549a15c67211a8ef1dc481792e4b106fde2e2d
S Campbell <50303034+Stccampbell@users.noreply.github.com>
2020-09-03 12:51:40 -0500

    Update followers-router.js

--- routes/followers-router.js
+++ routes/followers-router.js
@@ -6,11 +6,8 @@ const authHelpers = require('../services/auth/auth-helpers');
 followersRoutes.get('/follower/', authHelpers.loginRequired, followersController.indexFollowers);
 followersRoutes.get('/followed/', authHelpers.loginRequired, followersController.indexFollowed);
 
-<<<<<<< HEAD
-=======
 followersRoutes.get('/followed/checkFollowed/:id', authHelpers.loginRequired, followersController.indexFollowed);
 
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
 followersRoutes.post('/follower', authHelpers.loginRequired, followersController.create);
 followersRoutes.delete('/follower/:id', authHelpers.loginRequired, followersController.delete);
 
