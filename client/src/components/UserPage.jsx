6790a8f9baed82e4ed40af7e8c333f9923e7996b
S Campbell <50303034+Stccampbell@users.noreply.github.com>
2020-09-03 12:51:09 -0500

    Update UserPage.jsx

--- client/src/components/UserPage.jsx
+++ client/src/components/UserPage.jsx
@@ -9,24 +9,12 @@ class UserPage extends Component {
             outfits: [],
             dataLoaded: false,
             isFollowing: false, //need to add some backend stuff so that you can tell if you are following
-<<<<<<< HEAD
-=======
             followInstance: null,
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
           }
     }
 
     componentDidMount(){
         if(this.props.otherUser){
-<<<<<<< HEAD
-            this.getAllOutfits()
-        }
-    }
-
-    //check if following method here
-
-    follow = () => {
-=======
             this.getAllOutfits();
             this.checkFollowing();
         }
@@ -60,23 +48,12 @@ class UserPage extends Component {
 
     follow = () => {
         console.log(this.props.user.id)
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
         fetch(`/api/followerList/follower/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             credentials: 'include',
-<<<<<<< HEAD
-            body: {
-                'followed_id': this.props.user.id,
-            },
-        }).then(res => res.json())
-            .then(res => {
-                this.setState({
-                    isFollowing: true,
-                })
-=======
             body: JSON.stringify({'followed_id': this.props.user.id,}),
         }).then(res => res.json())
             .then(res => {
@@ -84,48 +61,29 @@ class UserPage extends Component {
                 //     isFollowing: true,
                 // })
                 this.checkFollowing();
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
             }).catch(err => console.log(err));
         }
 
     unFollow = () => {
-<<<<<<< HEAD
-        fetch(`/api/followerList/follower/${this.props.user.id}`, {
-=======
         fetch(`/api/followerList/follower/${this.state.followInstance}`, {
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
             method: 'DELETE',
         })
         .then(res => res.json())
         .then(res => {
-<<<<<<< HEAD
-            this.state({
-=======
             this.setState({
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
                 followingLoaded: false
             })
         })
         .then(res => {
-<<<<<<< HEAD
-            this.setState({
-                isFollowing: false,
-            })
-=======
             // this.setState({
             //     isFollowing: false,
             // })
             this.checkFollowing();
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
         }).catch(err => console.log(err))
     }
 
     getAllOutfits = () => {
-<<<<<<< HEAD
-        fetch('/api/outfits/user', 
-=======
-        fetch(`/api/outfits/user/${this.props.user.id}`, 
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
+        fetch(`/api/outfits/user/${this.props.user.id}`,
         { credentials: 'include' })
             .then(res => res.json())
             .then(res => {
@@ -145,38 +103,21 @@ class UserPage extends Component {
         if(this.props.otherUser){
             return <div>
                 {/* profile picture */}
-                
-<<<<<<< HEAD
-                <h1>Hello {this.props.user.username}!</h1>
-=======
                 <h1>{this.props.user.username}!</h1>
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
 
                 {/* bio */}
 
                 <button 
-<<<<<<< HEAD
-                    onClick={this.state.isFollowing ? () => this.unFollow : () => this.follow()}>
-                    {this.state.isFollowing ? "Unfollow" : "Follow"}
-                </button>
-                
-                {this.state.outfits.map((value, i) => {
-=======
                     onClick={this.state.isFollowing ? () => this.unFollow() : () => this.follow()}>
                     {this.state.isFollowing ? "Unfollow" : "Follow"}
                 </button>
                 
                 {this.state.dataLoaded ? this.state.outfits.map((value, i) => {
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
                     return <div key={i}> 
                         <p>{value.description}</p>
                         {/* addToCart */}
                     </div>})
-<<<<<<< HEAD
-                }
-=======
                 : <p>Loading...</p>}
->>>>>>> 2dff372290d9396f6fa976c5203e51914729fe98
 
             </div>
         }
