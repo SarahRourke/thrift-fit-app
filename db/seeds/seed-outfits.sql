7209bfb09f4f47697cfc0d968f55647ff4c265db
Shirley Hastier <shastier@yahoo.com>
2020-09-04 13:10:55 -0500

    BackEnd-Seed added 10 outfits, men

---
+++ db/seeds/seed-outfits.sql
@@ -0,0 +1,12 @@
+INSERT INTO outfits (user_id, description, img_url, price)
+VALUES
+
+(1, 'Work Fall Shirt', 'https://i.imgur.com/H1CgZpj.jpg', 10.00),
+(1, 'White sneakers, men', 'https://i.imgur.com/YHnqr5z.jpg',45.00),
+(1, 'Striped T-Shirt, men', 'https://i.imgur.com/MkAm5Gx.jpg', 40.00),
+(1, 'Slim regular pant, men', 'https://i.imgur.com/t9jP08e.png', 25.00),
+(1, 'Short, T-Shirt, men', 'https://i.imgur.com/iNzzRzK.jpg', 30.00),
+(1, 'Short, T-Shirt with a collar, men', 'https://i.imgur.com/vpktuR4.jpg', 45.00),
+(1, 'Graphic T-Shirt Jacket, men', 'https://i.imgur.com/lTlA0pN.jpg', 50.00),
+(1, 'Graphic T-Shirt duo', 'https://i.imgur.com/JRwpyWP.jpg', 35.00),
+(1, 'Blue Work Fall Shirt, men', 'https://i.imgur.com/0U9NIku.jpg', 10.00);
